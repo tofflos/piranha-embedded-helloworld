ffa9eec9049a23d5c8d8e2ddf31185c4489ad06b
Erik Östlund <erik.ostlund@gmail.com>
2020-09-20 10:14:50 +0200

    Removed license and author comments imported from IDE template

--- src/main/java/com/example/HelloWorldServlet.java
+++ src/main/java/com/example/HelloWorldServlet.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.example;
 
 import java.io.IOException;
@@ -11,10 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Erik
- */
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
